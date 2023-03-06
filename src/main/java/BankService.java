f0259e77e8e64d9e07526f553526a468bb34e63c
Michael Jiles <michaeljilesjr@yahoo.com>
2023-03-06 14:21:56 +0000

    Bank Account

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -1,3 +1,5 @@
+import kotlin.coroutines.ContinuationInterceptor;
+
 /**
  * This service class is intended to contain the basic logic for deposits and withdrawls to a bank account
  * Separating menu logic and bank logic is good for two reasons: it makes both classes as simple
@@ -23,6 +25,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
+        balance += amount;
 
     }
 
@@ -32,7 +35,10 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if(balance > 0 && (balance - amount) >= 0)
+        {
+            balance -= amount;
+        }
     }
 
     /**
@@ -40,6 +46,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
